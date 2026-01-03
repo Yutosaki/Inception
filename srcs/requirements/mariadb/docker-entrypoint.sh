fef7a42691e1f42c5777ee442225ccff0f6729a4
Yutosaki <sasayu8190@gmail.com>
2026-01-03 15:02:02 +0900

    fix: change mariadb command to mysql command coz there aren't those command in debian

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -3,13 +3,13 @@
 set -e
 
 echo '[mariadb-entrypoint] First-time setup: initializing database...'
-mariadb-install-db --user=mysql > /dev/null
+mysql_install_db --user=mysql > /dev/null
 
 echo '[mariadb-entrypoint] Starting temporary MariaDB...'
-mariadb-safe --nowatch & sleep 5
+mysqld_safe --nowatch & sleep 5
 
 echo '[mariadb-entrypoint] Creating database and user...'
-mariadb -u root <<EOF
+mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
@@ -17,7 +17,7 @@ FLUSH PRIVILEGES;
 EOF
 
 echo '[mariadb-entrypoint] Stopping temporary MariaDB...'
-mariadb-admin shutdown
+mysqladmin shutdown
 
 echo '[mariadb-entrypoint] Starting MariaDB server...'
 exec "$@"
