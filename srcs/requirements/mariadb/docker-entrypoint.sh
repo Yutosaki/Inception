1f38c5350e0542891a56c5b6918b86932a5c0976
Yutosaki <sasayu8190@gmail.com>
2026-01-06 18:07:02 +0900

    feat: add entry shell in mariadb

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,23 +1,42 @@
-#!/bin/bash
-
+#!/bin/sh
 set -e
 
-echo '[mariadb-entrypoint] First-time setup: initializing database...'
-mysql_install_db --user=mysql > /dev/null
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /var/run/mysqld
 
-echo '[mariadb-entrypoint] Starting temporary MariaDB...'
-mysqld_safe --nowatch & sleep 5
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo '[mariadb-entrypoint] First-time setup: initializing database...'
 
-echo '[mariadb-entrypoint] Creating database and user...'
-mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+    echo '[mariadb-entrypoint] Starting temporary MariaDB...'
+    mysqld_safe --datadir=/var/lib/mysql &
+    pid="$!"
+
+    echo '[mariadb-entrypoint] Waiting for MariaDB to start...'
+    until mysqladmin ping >/dev/null 2>&1; do
+        sleep 1
+    done
+
+    echo '[mariadb-entrypoint] Configuring Database...'
+
+    mysql -u root <<EOF
+    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
 EOF
 
 echo '[mariadb-entrypoint] Stopping temporary MariaDB...'
-mysqladmin shutdown
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+wait "$pid"
+echo '[mariadb-entrypoint] Setup finished.'
+fi
 
 echo '[mariadb-entrypoint] Starting MariaDB server...'
-exec "$@"
+
+exec mariadbd --datadir=/var/lib/mysql --bind-address=0.0.0.0
